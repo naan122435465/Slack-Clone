547858dd6109441d08c341203b4a433f74b41fcb
naan122435465 <lenak45a@gmail.com>
2024-12-06 15:04:24 +0700

    Toolbar component

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -25,9 +25,28 @@ export const create = mutation({
     return workspaceId;
   }
 });
+
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("workspaces").collect();
   }
 });
+
+export const getById = query({
+  args:{
+    id: v.id("workspaces")
+  },
+  handler: async( ctx, args)=>{
+
+    const userId = await auth.getUserId(ctx);
+
+    if(!userId){
+      throw new Error("Unauthozired")
+    }
+
+     return await ctx.db.get(args.id);
+
+  },
+})
+
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,18 @@
-interface WorkspaceIdPageProps {
-    params:{
-        workspaceId:string
-    }
-}
+
+"use client"
+
+import { useGetWorkSpace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 
-const WorkspaceIdPage = ({params}:WorkspaceIdPageProps) => {
 
 
+const WorkspaceIdPage = () => {
+    const workspaceId = useWorkspaceId()
+    const {data} = useGetWorkSpace({id: workspaceId});
     return (
         <div>
-            ID:{params.workspaceId}
+            Data:{JSON.stringify(data)}
         </div>
       );
 }
