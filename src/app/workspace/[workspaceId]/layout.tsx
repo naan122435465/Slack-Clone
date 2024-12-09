580dcad6efae831ce8443ab86ad80b2086dd8fec
naan122435465 <lenak45a@gmail.com>
2024-12-09 16:20:58 +0700

    Workspace Header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 
 interface WorkspaceIdLayoutProps {
@@ -14,7 +16,24 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
             <Toolbar />
             <div className="flex h-[calc(100vh-40px)]">
                 <Sidebar />
-                {children}
+                <ResizablePanelGroup
+                    direction="horizontal"
+                    autoSave="ca-workspace-layout"
+                >
+                    <ResizablePanel
+                    defaultSize={20}
+                    minSize={11}
+                    className = "bg-[#5E2C5F]"
+                    >
+                      <WorkspaceSidebar />
+                    </ResizablePanel>
+                    <ResizableHandle  withHandle/>
+                    <ResizablePanel minSize={20}>
+
+                        {children}
+                    </ResizablePanel>
+
+                </ResizablePanelGroup>
 
             </div>
         </div>
