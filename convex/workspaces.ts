736e9a259606ca302c5b52eb7027def5989ef2fc
naan122435465 <lenak45a@gmail.com>
2024-12-09 10:50:39 +0700

    Workspace Members

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,6 +2,17 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
 
+
+const generateCode = () => {
+  const code = Array.from(
+    {
+      length: 6
+    },
+    () =>
+      "0123456789abcdefghijlklmnopqrstuwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+  return code;
+};
 export const create = mutation({
   args: {
     name: v.string()
@@ -13,7 +24,7 @@ export const create = mutation({
     }
 
     // TODO: Create a proper method later
-    const joinCode = "123456";
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
@@ -21,6 +32,11 @@ export const create = mutation({
       joinCode
     });
 
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin"
+    });
 
     return workspaceId;
   }
@@ -29,23 +45,57 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+
+    return workspaces;
   }
 });
 
 export const getById = query({
-  args:{
+  args: {
     id: v.id("workspaces")
   },
-  handler: async( ctx, args)=>{
+  handler: async (ctx, args) => {
 
     const userId = await auth.getUserId(ctx);
 
-    if(!userId){
+    if (!userId) {
       throw new Error("Unauthozired")
     }
 
-     return await ctx.db.get(args.id);
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspcace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+    if (!member) {
+      return null;
+    }
+
+    return await ctx.db.get(args.id);
 
   },
 })
