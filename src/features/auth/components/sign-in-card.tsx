9cb6d7303941233e084af42d48d898e747dd8767
naan122435465 <lenak45a@gmail.com>
2025-01-07 15:40:24 +0700

    workspace join page

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -36,11 +36,12 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                 setPending(false);
             })
     }
+    console.log('process.env.JWT_PRIVATE_KEY',process.env.JWT_PRIVATE_KEY)
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0">
                 <CardTitle>
-                    Login to continue
+                    Login to continue 
                 </CardTitle>
                 <CardDescription>
                     User your email or another service to continue
